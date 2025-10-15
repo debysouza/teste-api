adc653c4f5d4f76db77e2c07c42c0df142c8d9c6
debysouza <debsdebbie90@gmail.com>
2025-10-15 10:31:05 -0300

    commit inicial

--- src/main/java/com/senai/infob/aula/configs/Cors.java
+++ src/main/java/com/senai/infob/aula/configs/Cors.java
@@ -23,7 +23,9 @@ public class Cors {
             "http://127.0.0.1:3000",
             "http://127.0.0.1:3001",
             "http://127.0.0.1:80",
-            "http://localhost:80"
+            "http://localhost:80",
+            "http://127.0.0.1:5500",
+            "http://localhost:5500"
         );
         config.setAllowedOrigins(allowedOrigins);
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"));
